a2924c5e3cda462fb1031af4860dc6dd808676e0
HRamil <ramilhasanov2000@gmail.com>
2025-03-15 23:45:31 +0300

    deploy

--- src/app/admin/certificate/page.tsx
+++ src/app/admin/certificate/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import { useState } from "react";
 import Swal from "sweetalert2";
 import SendCertificateForm from "@/component/sendCertificateForm";
 import SendCertificateTable from "@/component/sendCertificateTable";
